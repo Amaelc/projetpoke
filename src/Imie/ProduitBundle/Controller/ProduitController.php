51d595de029e23ce4c1f32c31e89d77cba311ff2
Radu <emi_alpina@yahoo.com>
2016-02-24 16:50:57 +0100

    Signed-off-by: Radu <emi_alpina@yahoo.com>

---
+++ src/Imie/ProduitBundle/Controller/ProduitController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Imie\ProduitBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ProduitController extends Controller
+{
+    
+    public function indexAction()
+    {
+        return $this->render('ImieProduitBundle:Produit:index.html.twig');
+    }
+    
+    public function voirAction($id)
+    {
+    return $this->render('ImieProduitBundle:Produit:voir.html.twig', array('id' => $id));
+    }
+}
