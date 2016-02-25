8de397c4c325e95f1551512c3c8ea5117a446682
Amael COLAS <colas.amael@gmail.com>
2016-02-25 16:29:55 +0100

    modif vue detail produit routes et methodes

--- src/Imie/ProduitBundle/Controller/StockController.php
+++ src/Imie/ProduitBundle/Controller/StockController.php
@@ -11,6 +11,8 @@ use Imie\ProduitBundle\Entity\Stock;
 use Imie\ProduitBundle\Repository\ProduitRepository;
 use Imie\ProduitBundle\Repository\StockRepository;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route; 
+
 class StockController extends Controller
 {  
     public function indexAction(){
@@ -39,4 +41,43 @@ class StockController extends Controller
         return $this->render('ImieProduitBundle:Stock:index.html.twig', $variables);
         
     }
+    
+    
+ 
+    /** 
+     * // pour datatable
+     */ 
+    public function paginateAction(Request $request) 
+    { 
+        $length = $request->get('length'); 
+        $length = $length && ($length!=-1)?$length:0; 
+ 
+        $start = $request->get('start'); 
+        $start = $length?($start && ($start!=-1)?$start:0)/$length:0; 
+ 
+        $search = $request->get('search'); 
+        $filters = [ 
+            'query' => @$search['value'] 
+        ]; 
+ 
+        //$users = $this->getDoctrine()->getRepository('AcmeDemoBundle:User')->search(   
+        $stocks =  $this->getDoctrine()->getRepository('ImieProduitBundle:Stock')->search( 
+            $filters, $start, $length 
+        );
+
+        $output = array( 
+            'data' => array(), 
+            'recordsFiltered' => count($this->getDoctrine()->getRepository('ImieProduitBundle:Stock')->search($filters, 0, false)), 
+            'recordsTotal' => count($this->getDoctrine()->getRepository('ImieProduitBundle:Stock')->search(array(), 0, false)) 
+        ); 
+ 
+        foreach ($stocks as $stock) { 
+            $output['data'][] = [ 
+                'id' => $stock->getId(), 
+                'nom' => $user->getName()
+            ]; 
+        } 
+ 
+        return new Response(json_encode($output), 200, ['Content-Type' => 'application/json']); 
+    } 
 }    
